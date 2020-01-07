7775897c71aa3732edf144db6a35ee662679586f
xuanze <chenxz@wdcloud.cc>
2020-01-07 18:31:34 +0800

    1.提交修改；

--- RxSwiftTest/UIImagePickerController+RxCreate.swift
+++ RxSwiftTest/UIImagePickerController+RxCreate.swift
@@ -10,8 +10,39 @@ import Foundation
 import RxCocoa
 import RxSwift
 
+public class RxImagePickerDelegateProxy: DelegateProxy<UIImagePickerController, UIImagePickerControllerDelegate & UINavigationControllerDelegate>, DelegateProxyType, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    public init (imagePicker: UIImagePickerController) {
+        super.init(parentObject: imagePicker, delegateProxy: RxImagePickerDelegateProxy.self)
+    }
+    
+    public static func registerKnownImplementations() {
+        self.register { (parent) -> RxImagePickerDelegateProxy in
+            return RxImagePickerDelegateProxy(imagePicker: parent)
+        }
+    }
+    
+    public static func currentDelegate(for object: UIImagePickerController) -> (UIImagePickerControllerDelegate & UINavigationControllerDelegate)? {
+        return object.delegate
+    }
+    
+    public static func setCurrentDelegate(_ delegate: (UIImagePickerControllerDelegate & UINavigationControllerDelegate)?, to object: UIImagePickerController) {
+        object.delegate = delegate
+    }
+    
+    
+}
+
 extension Reactive where Base: UIImagePickerController {
-    public var pickerDelegate = DelegateProxy
+    
+    public var pickerDelegate: DelegateProxy<UIImagePickerController, UIImagePickerControllerDelegate & UINavigationControllerDelegate> {
+        return RxImagePickerDelegateProxy.proxy(for: base)
+    }
+    
+    public var didFinishPickingMediaWithInfo: Observable<[String: AnyObject]> {
+        return pickerDelegate.methodInvoked(#selector(UIImagePickerControllerDelegate.imagePickerController(_:didFinishPickingMediaWithInfo:))).map { (<#[Any]#>) -> Result in
+            return 
+        }
+    }
     
 //    static func createWithParent(parent: UIViewController?, animated: Bool = true, configureImagePicker: @escaping (UIImagePickerController) throws -> Void = {x in}) -> Observable<UIImagePickerController> {
 //        return Observable.create { (observer) -> Disposable in
